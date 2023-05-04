63d79ae3f9aa6a7008332792f938f754e393bca5
Alexandre Zamberlan <alexz@ufn.edu.br>
2023-05-04 17:03:41 -0300

    correção do create app triagem para paciente

--- projeto/triagem/forms.py
+++ projeto/triagem/forms.py
@@ -9,11 +9,11 @@ from .models import Triagem
 
 class TriagemFormCreate(forms.ModelForm):
     responsavel = forms.ModelChoiceField(label='Responsável pela triagem *', queryset=Usuario.tecnicos_enfermeiros.all())
-    paciente_ativo = forms.ModelChoiceField(label='Paciente *', queryset=Paciente.pacientes_ativos.all())
+    paciente2 = forms.ModelChoiceField(label='Paciente *', queryset=Paciente.pacientes_ativos.all())
 
     class Meta:
         model = Triagem
-        fields = ['responsavel', 'unidade', 'paciente_ativo', 'data_nascimento', 'altura', 'peso',
+        fields = ['responsavel', 'unidade', 'paciente2', 'data_nascimento', 'altura', 'peso',
               'tem_febre', 'tem_dor_cabeca', 'tem_secrecao_espirros', 'tem_irritacao_garganta',
               'tem_tosse', 'tem_dificuldade_respiratoria', 'tem_dores_corpo', 'tem_diarreia',
               'viajou', 'contatou']
